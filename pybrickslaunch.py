897640d097829824013c4590abc846d99c6f6580
PetroBlitz841 <petroblitz.841@gmail.com>
2024-11-27 16:32:48 +0200

    renaming & other things

--- pybrickslaunch.py
+++ pybrickslaunch.py
@@ -1,4 +1,4 @@
-HUB_NAME = "Pybricks Hub"
+HUB_NAME = "Controller5"
 
 import os
 import subprocess
--- robot-tests.py
+++ robot-tests.py
@@ -5,34 +5,57 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, hub_menu
 from pybricks.tools import hub_menu
 
-hub = PrimeHub()
 
+# Hardware definitions
+hub = PrimeHub()
 left_wheel = Motor(Port.C)
 right_wheel = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 left_arm = Motor(Port.E)
 right_arm = Motor(Port.D)
-fast_change_sensor = ColorSensor(Port.F)
+run_sensor = ColorSensor(Port.F)
 map_sensor = ColorSensor(Port.A)
+wheels = DriveBase(left_wheel, right_wheel, wheel_diameter=62.4, axle_track=10.6)
 
-yosi_jr_jr = DriveBase(left_wheel, right_wheel, wheel_diameter=62.4, axle_track=10.6)
+run_colors = (Color.BLACK, Color.GREEN, Color.WHITE, Color.YELLOW, Color.RED)
+run_sensor.detectable_colors(run_colors)
 
-halifacolors = (Color.BLACK, Color.GREEN, Color.WHITE, Color.YELLOW, Color.RED)
-fast_change_sensor.detectable_colors(halifacolors)
 
-halifa = fast_change_sensor.color()
-if halifa == Color.BLACK:
+# Runs =================================================================================
+def black_run():
     hub.display.number(1)
-    yosi_jr_jr.straight(distance=100)
-    right_arm.turn(50)
-elif halifa == Color.GREEN:
-    hub.display.number(2)
-    yosi_jr_jr.straight(distance=300)
-elif halifa == Color.WHITE:
-    hub.display.number(3)
-    yosi_jr_jr.straight(distance=500)
-elif halifa == Color.YELLOW:
-    hub.display.number(4)
-    yosi_jr_jr.straight(distance=600)
-elif halifa == Color.RED:
-    hub.display.number(5)
-    yosi_jr_jr.straight(distance=700)
+    wheels.settings(300)
+
+    wheels.straight(distance=200)
+    wheels.straight(distance=-200)
+
+
+def green_run():
+    wheels.settings()
+    pass
+
+
+def white_run():
+    wheels.settings()
+    pass
+
+
+def yellow_run():
+    wheels.settings()
+    pass
+
+
+def red_run():
+    wheels.settings()
+    pass
+
+
+color_options = {
+    Color.BLACK: black_run,
+    Color.GREEN: green_run,
+    Color.WHITE: white_run,
+    Color.YELLOW: yellow_run,
+    Color.YELLOW: red_run,
+}
+halifa = run_sensor.color()
+# print("current: ", hub.battery.current(), "voltage: ", hub.battery.voltage())
+color_options[halifa]()
